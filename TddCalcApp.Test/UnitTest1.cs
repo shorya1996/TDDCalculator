4be1c7b4c79bd7732016b6cc8f0eec74b6324df3
shorya1996 <ssharma1996@live.com>
2019-09-12 21:56:22 -0700

    Initial Add Test Failed

--- TddCalcApp.Test/UnitTest1.cs
+++ TddCalcApp.Test/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddCalcApp.library;
 
 namespace TddCalcApp.Test
 {
@@ -9,6 +10,12 @@ namespace TddCalcApp.Test
         [TestMethod]
         public void TestMethod1()
         {
+            Calculator Testcalculator = new Calculator();
+            int num1 = 1;
+            int num2 = 2;
+            int result = Testcalculator.Add(num1, num2);
+            Assert.AreEqual(3, result, "Testing two Integer 3 and 2");
+
         }
     }
 }
